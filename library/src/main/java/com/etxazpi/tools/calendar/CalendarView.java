3b4a8c8c21b328cbf9686fbaacf96eb2e5863f35
Xabier Carmona <xabisolo@gmail.com>
2019-11-18 01:21:11 +0100

    Fixed minor bug

--- library/src/main/java/com/etxazpi/tools/calendar/CalendarView.java
+++ library/src/main/java/com/etxazpi/tools/calendar/CalendarView.java
@@ -437,13 +437,13 @@ public class CalendarView extends LinearLayout implements View.OnTouchListener, 
         removeSelection(selectedDay);
         addSelection(selectedDayCalendar);
 
-        selectedDay = selectedDayCalendar;
+        selectedDay = (Calendar) selectedDayCalendar.clone();
         calendarInUse.set(Calendar.YEAR, selectedDay.get(Calendar.YEAR));
         calendarInUse.set(Calendar.MONTH, selectedDay.get(Calendar.MONTH));
         calendarInUse.set(Calendar.DAY_OF_MONTH, selectedDay.get(Calendar.DAY_OF_MONTH));
 
         if (calendarListener != null) {
-            calendarListener.onDaySeledted(calendarInUse);
+            calendarListener.onDaySeledted(selectedDay);
         }
     }
 
